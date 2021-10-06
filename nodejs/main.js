3890e2963079ec4e31b12dab8af4ec72ccf3cf91
yumi <dldbal123@google.com>
2021-10-06 21:46:53 +0900

    update
    JavaScript 파일 목록 불러오기 기능 추가

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,38 +5,89 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+    
+    if(pathname === '/') {
+      if(queryData.id === undefined){
+        
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          
+        //   var list = `<ul>
+        //   <li><a href="/?id=HTML">HTML</a></li>
+        //   <li><a href="/?id=CSS">CSS</a></li>
+        //   <li><a href="/?id=JavaScript">JavaScript</a></li>
+        // </ul>`
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list +  `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list +  `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          // console.log(__dirname + url); // 화면으로 나타나는 현재 경로 출력
+          //response.end(fs.readFileSync(__dirname + url));  
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    // console.log(__dirname + url); // 화면으로 나타나는 현재 경로 출력
-    //response.end(fs.readFileSync(__dirname + url));
-    })
+    
  
 });
 app.listen(3000);
--- nodejs/syntax/loop.js
+++ nodejs/syntax/loop.js
@@ -1,13 +1,11 @@
-var args = process.argv;
-console.log(args[2])
-
 console.log('A');
 console.log('B');
-
-if(args[2] == '1'){
+ 
+var i = 0;
+while(i < 2){
   console.log('C1');
-} else {
   console.log('C2');
+  i = i + 1;
 }
-
+ 
 console.log('D');
